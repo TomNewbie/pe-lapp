b74691bf96b131302323adbb84a130b813ac25f5
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2023-03-20 15:25:54 +0700

    add type jwt

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express, {Request, Response, NextFunction} from "express";
-import { OAuth2Client } from "google-auth-library"
+import express, { Request, Response, NextFunction } from "express";
+import { OAuth2Client } from "google-auth-library";
 import jwt from "jsonwebtoken";
-import 'express-async-errors';
-import { errorHandler } from './utils/middleware';
+import "express-async-errors";
+import { errorHandler } from "./utils/middleware";
 
 const app = express();
 const port = process.env.PORT;
@@ -12,20 +12,21 @@ const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 const callback_url = "http://localhost:3000/auth/google/callback";
 const jwt_secret = process.env.JWT_SECRET;
-const client = new OAuth2Client(
-  client_id,
-  client_secret,
-  callback_url
-);
+const client = new OAuth2Client(client_id, client_secret, callback_url);
 
 // Middleware to check if the JWT token is valid
-const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
+const authenticateJWT = (
+  req: Request & { user?: string | jwt.JwtPayload },
+  res: Response,
+  next: NextFunction
+) => {
   const authHeader = req.headers.authorization;
 
   if (authHeader) {
     const token = authHeader.split(" ")[1];
 
     jwt.verify(token, jwt_secret as string, (err, user) => {
+      console.log(user);
       if (err) {
         return res.sendStatus(403);
       }
@@ -39,17 +40,17 @@ const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
 };
 
 // Route for Google OAuth login
-app.get('/auth/google', (req: Request, res: Response) => {
-    const url = client.generateAuthUrl({
-      access_type: 'offline',
-      scope: ['profile', 'email'],
-    });
-  
-    res.redirect(url);
+app.get("/auth/google", (req: Request, res: Response) => {
+  const url = client.generateAuthUrl({
+    access_type: "offline",
+    scope: ["profile", "email"],
+  });
+
+  res.redirect(url);
 });
 
 // Route for Google OAuth callback
-app.get('/auth/google/callback', async (req: Request, res: Response) => {
+app.get("/auth/google/callback", async (req: Request, res: Response) => {
   const { tokens } = await client.getToken(req.query.code as string);
   client.setCredentials(tokens);
 
@@ -67,8 +68,8 @@ app.get('/auth/google/callback', async (req: Request, res: Response) => {
 });
 
 // Protected route that requires JWT token authentication
-app.get('/protected', authenticateJWT, (req: Request, res: Response) => {
-    res.send('You have access to the protected resource!');
+app.get("/protected", authenticateJWT, (req: Request, res: Response) => {
+  res.send("You have access to the protected resource!");
 });
 
 app.use(errorHandler);
