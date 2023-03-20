d89fce4e81b772e14b0220addb50599c7a271a02
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-03-20 15:10:23 +0700

    Rename user.ts to user.d.ts and add IGoogleUser

--- server/src/api/types/user.d.ts
+++ server/src/api/types/user.d.ts
@@ -1,10 +1,13 @@
-export interface IUser {
+export interface IGoogleUser {
   name: string;
   email: string;
+  avatar: string;
+}
+
+export interface IUser extends IGoogleUser {
   role: Role;
   major?: string;
   phone_number?: string;
-  avatar?: string;
 }
 
 export type Role = "student" | "teacher";
