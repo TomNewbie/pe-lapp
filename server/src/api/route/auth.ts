e4446604386a0d8393825f241207815f92613f8b
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-03-20 15:21:22 +0700

    Remove comments in src/api/route/auth.ts

--- server/src/api/route/auth.ts
+++ server/src/api/route/auth.ts
@@ -1,7 +1,5 @@
 import { Router } from "express";
 import { authController } from "../controller/auth";
 const router = Router();
-// authController.checkNewUser, authController.logi
-// router.use("/login", loginController);
 router.get("/auth/google/callback", authController.login);
 export { router as authRouter };
