2cb7cd916abf61e93bf1d2f44dec04351afdad99
pndquyynh <17937@student.vgu.edu.vn>
2023-03-21 20:16:35 +0700

    Update images of course card

--- lapp/src/App.js
+++ lapp/src/App.js
@@ -1,9 +1,14 @@
 import { Footer } from "./components";
 import { Login } from "./pages/common";
+import { CoursecardStudent } from "./components";
+import {NavbarStudent} from "./components";
+
 function App() {
   return (
     <div className="App">
       <Login></Login>
+      <CoursecardStudent/>
+
     </div>
   );
 }
---
+++ lapp/src/components/CoursecardStudent/index.js
@@ -0,0 +1,13 @@
+const CoursecardStudent = () => {
+    return ( 
+        <div> 
+           <h1>Course Name</h1> 
+           <img src="/Coursecard_img/human.png" alt="Participants: " />
+           {/* <p> Numbers of Participants </p> */}
+           <img src="/Coursecard_img/progress.png" alt="Progress: " />
+           {/* <p>Progress percentage</p> */}
+        </div>
+     );
+}
+ 
+export default CoursecardStudent;
