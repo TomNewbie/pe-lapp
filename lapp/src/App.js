8d9f1c275ea1929e748e449ecbf61804af676cc9
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-03-24 22:34:39 +0700

    Update svg

--- lapp/src/App.js
+++ lapp/src/App.js
@@ -1,13 +1,13 @@
 // import { NavbarStudent } from "./components";
 // import { Login } from "./pages/common";
-import {Dropdown, Search_box} from "./components";
+import { SearchBox } from "./components";
 function App() {
   return (
     <div className="App">
       {/* <Login></Login> */}
       {/* <NavbarStudent></NavbarStudent> */}
       {/* <Dropdown></Dropdown> */}
-      <Search_box></Search_box>
+      <SearchBox />
     </div>
   );
 }
--- lapp/src/components/index.js
+++ lapp/src/components/index.js
@@ -1,8 +1,5 @@
 export { default as Footer } from "./Footer";
 export { default as NavbarStudent } from "./NavbarStudent";
 export { default as NavbarLecturer } from "./NavbarLecturer";
-export { default as Dropdown} from "./Dropdown";
-export { default as Search_box} from "./Search_box";
-export default {
-    components:{SearchIcon},
-};
+export { default as Dropdown } from "./Dropdown";
+export { default as SearchBox } from "./SearchBox";
