b362bbae8155245358d226b9f336cf3e3eec7d6d
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-03-25 22:45:32 +0700

    Update login page

--- lapp/src/pages/common/Login/index.js
+++ lapp/src/pages/common/Login/index.js
@@ -4,15 +4,23 @@ const Login = () => {
     <div>
       {/* Start: logo */}
       <div className="relative">
-        <div class="flex justify-between">
-          <div class="m-3">
-            <img src="/login/Logo.png" alt="logo" width={100} height={100} />
+        <div>
+          <div className="absolute top-2 ml-14">
+            <img src="/login/Logo.png" alt="logo" width={70} height={70} />
           </div>
-          <div>
+          <div className="absolute -top-[116px] -right-64 ">
             <img
-              src="/login/Ellipse.png"
+              src="/login/Ellipse1.png"
               alt="ellipse"
-              width={700}
+              width={852}
+              height={500}
+            />
+          </div>
+          <div className="absolute right-0 -top-[246px]">
+            <img
+              src="/login/Ellipse2.png"
+              alt="ellipse"
+              width={1140}
               height={500}
             />
           </div>
@@ -22,35 +30,64 @@ const Login = () => {
         {/* Start: Intro section */}
         <div class="absolute top-0">
           <div class="grid grid-cols-2">
-            <div>
-              <p class="text-center text-dongle text-7xl pt-36 text-black">
-                The best
+            <div className="pl-[50px] pt-44">
+              <p class="text-dongle text-8xl text-black text-bold pl-0">
+                The best learning
               </p>
-              <p class="text-center text-dongle text-7xl text-black">
-                learning management system
-              </p>
-              <p class="text-left text-2xl px-6 ml-10 text-black">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                rutrum ipsum metus, eget pretium ipsum blandit eu. Maecenas
-                ultrices eros non libero semper placerat eget eget urna.
-                Maecenas et orci diam. Sed quis urna sit amet magna tempor
-                viverra et a orci. Proin lacinia et est a laoreet. Lorem ipsum
-                dolor sit amet, consectetur adipiscing elit.
+              <p class="text-dongle text-8xl text-black text-bold pl-0">
+                management system
               </p>
               {/* Start: Log in Button */}
-              <div className="flex justify-center pt-12">
-                <button className="px-20 text-4xl text-black rounded-lg bg-pink1">
-                  Log in to Google
+              <div className="flex justify-center ">
+                <button className=" w-80 h-14 mt-6 rounded-lg border-2 border-[#CC6666] loginBtn">
+                  <span className="text-[40px] font-bold text-black opacity-100">
+                    Log in to Google
+                  </span>
                 </button>
               </div>
               {/* End: Log in Button */}
             </div>
-            <div class="h-12">
+            <div class="absolute right-2 top-[12px]">
               <img src="/login/image.svg" alt="image1" />
             </div>
           </div>
         </div>
         {/* End: Intro section */}
+
+        {/* Start: icon section */}
+        <div>
+          <div className="absolute top-36 left-[230px]">
+            <img
+              src="/login/airplane.png"
+              alt="airplane"
+              width={40}
+              height={40}
+            />
+          </div>
+
+          <div className="absolute top-40 left-[520px]">
+            <img src="/login/bling.png" alt="bling" width={40} height={40} />
+          </div>
+
+          <div className="absolute top-[360px] left-[30px] ">
+            <img src="/login/over.png" alt="over" width={60} height={60} />
+          </div>
+          <div className="absolute top-[460px] left-[140px]">
+            <img src="/login/under.png" alt="under" width={60} height={60} />
+          </div>
+          <div className="absolute top-[360px] left-[520px]">
+            <img src="/login/arrow.png" alt="arrow" width={120} height={120} />
+          </div>
+          <div className="absolute top-[460px] left-[480px]">
+            <img
+              src="/login/diamond.png"
+              alt="diamond"
+              width={30}
+              height={30}
+            />
+          </div>
+        </div>
+        {/* End: icon section */}
       </div>
       <div className="absolute bottom-0">
         <Footer></Footer>
