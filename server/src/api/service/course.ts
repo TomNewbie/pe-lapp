85e0bf451660663ee9b24ea3a2f7daeda31a5e2a
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2023-03-26 01:03:58 +0700

    add readme api at below

--- server/src/api/service/course.ts
+++ server/src/api/service/course.ts
@@ -1,5 +1,6 @@
 import { Types } from "mongoose";
 import { Course } from "../model/course";
+import { RichText } from "../model/richtext";
 import { ICourse } from "../types/course";
 
 export const getAllCourse = async (
@@ -9,11 +10,14 @@ export const getAllCourse = async (
   return courses;
 };
 
-export const getCourseById = async (
+export const getDetailCourse = async (
   id: Types.ObjectId
-): Promise<ICourse[] | null> => {
-  const courses = await Course.find({ content: id });
-  return courses;
+): Promise<string | null> => {
+  const course = await RichText.findById({ id });
+  if (!course) {
+    throw new Error("Course not found");
+  }
+  return course.content;
 };
 
 export const createCourse = async (course: ICourse) => {
