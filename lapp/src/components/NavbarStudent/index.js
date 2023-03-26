5dec9a32f0dffc0cf4e5a82b331d054e5b262c42
linh <khanhlinhvng>
2023-03-26 21:58:14 +0700

    new update

--- lapp/src/components/NavbarStudent/index.js
+++ lapp/src/components/NavbarStudent/index.js
@@ -1,36 +1,38 @@
+import SearchBox from "../SearchBox";
+
 const NavbarStudent = () => {
   return (
   <header className="bg-[#CC6666]/20">
-      <nav className="flex justify-between items-center w-[92%] mx-auto">
-        <div>
+      <nav className="relative flex items-center">
+        <div className="ml-8">
           <img src="/navbar_img/Logo.png" alt ="logo"/>
         </div>
-        <div className="">
+        <div className="absolute ml-40">
           <ul className="flex items-center gap-[3vw]">
             <li>
-              <a className="hover:text-red-400" href="#" >Home</a>
+              <a className="text-dongle hover:text-white" href="#" >Home</a>
             </li>
             <li>
-              <a className="hover:text-red-400" href="#" >Cource</a>
+              <a className="text-dongle hover:text-white" href="#" >Courses</a>
             </li>
             <li>
-              <a className="hover:text-red-400" href="#" >Lecturer</a>
+              <a className="text-dongle hover:text-white" href="#" >Lecturers</a>
             </li>
           </ul>
         </div>
-        <div>
+        <div className="absolute ml-96">
           <div className="search_box">
-          <input className="border-2 border-black" placeholder="Search"></input>
+            <SearchBox></SearchBox>
           </div>
         </div>
-        <div>
+        <div className="absolute right-40">
           <div className ="text">Hi Student ID,</div>
         </div>
-        <div>
+        <div className="absolute right-28">
           <img src="/navbar_img/profile_pic.png" alt ="profile_pic"/>
         </div>
-        <div>
-          <button className="bg-[#6E5F56] px-5 py-2 rounded-full hover:bg-[#F0EAE0]">Log out</button>
+        <div className="absolute right-0 mr-8">
+          <button className="text-white bg-[#B02B3B] px-5 py-2 rounded-full hover:bg-[#F0EAE0]">Log out</button>
         </div>
       </nav>
   </header>
