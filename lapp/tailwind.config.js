31f86e3225209a7d52082f1508044cce427820b2
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-03-26 18:03:51 +0700

    Update tailwind config

--- lapp/tailwind.config.js
+++ lapp/tailwind.config.js
@@ -6,13 +6,6 @@ module.exports = {
       fontFamily: {
         dongle: ["Dongle"],
       },
-      colors: {
-        orange1: "#FBA70E",
-        pink1: "#F1B2B2",
-        brown1: "#BD9E84",
-        blue1: "#93AEC1",
-        background: "#F0EAE0",
-      },
     },
   },
   plugins: [],
