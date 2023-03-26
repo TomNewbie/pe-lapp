12b3ad68a896d482e02f626b1e8a36895043affe
linh <khanhlinhvng>
2023-03-26 17:27:26 +0700

    new update

--- lapp/src/components/index.js
+++ lapp/src/components/index.js
@@ -2,4 +2,4 @@ export { default as Footer } from "./Footer";
 export { default as NavbarStudent } from "./NavbarStudent";
 export { default as NavbarLecturer } from "./NavbarLecturer";
 export { default as Dropdown} from "./Dropdown";
-export { default as Search_box} from "./Search_box";
+export { default as SearchBox} from "./SearchBox";
