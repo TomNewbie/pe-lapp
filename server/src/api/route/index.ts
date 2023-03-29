ea3c5b88d82d1e79265bfc0aa6a69a1997c3b59f
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-03-29 17:32:05 +0700

    Add catch all 404 for api route

--- server/src/api/route/index.ts
+++ server/src/api/route/index.ts
@@ -1,8 +1,12 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { authRouter } from "./auth";
 
 const router = Router();
 
 router.use(authRouter);
+
+router.all("*", (_: Request, res: Response) => {
+  res.sendStatus(404);
+});
 
 export { router };
