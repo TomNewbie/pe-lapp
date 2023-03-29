0471527d2391e8eb9c3e54034d052ba05065fccd
linh <khanhlinhvng>
2023-03-29 13:05:44 +0700

    new update

--- lapp/src/components/SearchBox/index.js
+++ lapp/src/components/SearchBox/index.js
@@ -1,19 +1,21 @@
-function SearchBox({size1,size2}) {
+function SearchBox({ px, py }) {
+  const style = `px-${px} py-${py} border-2 border-black rounded-xl text-xl`;
   return (
     <div>
-      <div className="relative flex items-center" width={size1} height={size2}>
-        <img src="/SearchBox/searchicon.svg" alt="" className="absolute ml-32"width={25} height={25} />
-        <input
-          type="text"
-          className="px-3 py-2 border-2 border-black rounded-xl text-xl"
-          placeholder="Search"
-        ></input>
+      <div className="relative flex items-center">
+        <img
+          src="/SearchBox/searchicon.svg"
+          alt=""
+          className="absolute ml-28"
+          width={25}
+          height={25}
+        />
+        <input type="text" className={style} placeholder=" Search"></input>
       </div>
     </div>
   );
 }
-export default function SearchBox1({size1,size2}){
-  return(
-    <SearchBox size1={200} size2={50}></SearchBox>
-  );
+export default function SearchBox1({ px, py }) {
+  return <SearchBox px={10} py={0}></SearchBox>;
 }
+
