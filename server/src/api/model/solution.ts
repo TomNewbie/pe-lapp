09facdb531e2d3770d59a667e98957bf5f010370
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2023-03-30 22:29:04 +0700

    Server: Add solution schema

---
+++ server/src/api/model/solution.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+export const solution = new Schema(
+  {
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+      required: true,
+    },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    file: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Solution = mongoose.model("Solution", solution);
