e4b0fc326dafe89b011d6821bc1bde050694eb1e
linh <khanhlinhvng>
2023-03-31 22:21:20 +0700

    new update

--- lapp/src/App.js
+++ lapp/src/App.js
@@ -1,13 +1,15 @@
 // import { NavbarStudent } from "./components";
 // import { Login } from "./pages/common";
-import { Dropdown, SearchBox } from "./components";
+// import { Dropdown, SearchBox } from "./components";
+import {File} from "./components";
 function App() {
   return (
     <div className="App">
       {/* <Login></Login> */}
       {/* <NavbarStudent></NavbarStudent> */}
       {/* <Dropdown></Dropdown> */}
-      <SearchBox></SearchBox>
+      {/* <SearchBox px={10} py={1}></SearchBox> */}
+      <File></File>
     </div>
   );
 }
---
+++ lapp/src/components/File/index.js
@@ -0,0 +1,19 @@
+const File = () => {
+    return (
+        <div>
+            <div className="w-3/4 h-20 rounded-2xl border-4 border-[#7F1734] hover:bg-[#F4C2C2]/20">
+                <div className="flex flex-row my-0.5">
+                    <img src="/File/file-icon.png" alt ="" 
+                         className="ml-4"></img>
+                    <ul className="ml-7">
+                        <li className="text-2xl font-normal">File</li>
+                        <li className="text-3xl font-semibold">File name</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+  };
+  
+  export default File;
+  
