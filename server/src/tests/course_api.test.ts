3aa0fcadc37a066c008eb68510d7904c53a8c3f6
NguyenLe1605 <lhdnguyen2002@gmail.com>
2023-04-04 15:56:16 +0700

    Add test for GET /api/courses

--- server/src/tests/course_api.test.ts
+++ server/src/tests/course_api.test.ts
@@ -1,34 +1,55 @@
 import { expect } from "chai";
-import * as mongoose from "mongoose";
 import supertest from "supertest";
 import app from "../app";
 import * as mongoServer from "./utils/memory_mongodb_helper";
-import {courses} from "./utils/initials"
+import {courses, students, lecturers} from "./utils/initials"
 import {Course} from "../api/model/course";
+import { Student, Lecturer } from "../api/model/user";
+import jwt from "jsonwebtoken";
 
 const api = supertest(app);
+const jwt_secret = process.env.JWT_SECRET;
 
 before(async () => {
     await mongoServer.connect();
-  });
+});
 
 beforeEach(async () => {
-await Course.deleteMany();
-await Course.insertMany(courses);
+    await Student.deleteMany();
+    await Lecturer.deleteMany();
+    await Course.deleteMany();
+    await Student.insertMany(students);
+    await Lecturer.insertMany(lecturers);
+    await Course.insertMany(courses);
 })
 
 after(async () => {
-await mongoServer.disconnect
+    await mongoServer.disconnect
 });
 
 describe("GET /api/courses", () => {
-
-  it("should return all courses", async () => {
+  it("should return all courses of the sent student", async () => {
+    const expectedCourses = [
+      {
+        name: "Course A",
+        picture: "http://example.com/image1.jpg",
+        lecturer_name: "Dr. Sarah Lee",
+        progress: 75,
+      },
+      {
+        name: "Course B",
+        picture: "http://example.com/image2.jpg",
+        lecturer_name: "Dr. Sarah Lee",
+        progress: 75,
+      },
+    ]
+    const student = students[0];
+    const accessToken = jwt.sign({email: student._id}, jwt_secret!);
     const res = await api
         .get("/api/courses")
+        .set('Authorization', `Bearer ${accessToken}`)
         .expect(200);
-
-    expect(res.status).to.equal(200);
     expect(res.body).to.have.lengthOf(2);
+    expect(res.body).to.have.deep.members(expectedCourses);
   });
 });
--- server/src/tests/utils/initials.ts
+++ server/src/tests/utils/initials.ts
@@ -1,68 +1,89 @@
+export const lecturers = [
+    {
+      name: "Dr. Sarah Lee",
+      _id: "sarahlee@example.com",
+      avatar: "https://example.com/avatar/sarahlee.jpg",
+      phone_number: "111-111-1111",
+      faculty: "Computer Science"
+    },
+    {
+      name: "Dr. Michael Johnson",
+      _id: "michaeljohnson@example.com",
+      avatar: "https://example.com/avatar/michaeljohnson.jpg",
+      phone_number: "222-222-2222",
+      faculty: "Business Administration"
+    },
+    {
+      name: "Dr. Lisa Chen",
+      _id: "lisachen@example.com",
+      avatar: "https://example.com/avatar/lisachen.jpg",
+      phone_number: "333-333-3333",
+      faculty: "Psychology"
+    }
+  ]
+
+export const students = [
+    {
+      name: "John Doe",
+      _id: "17028@student.vgu.edu.vn",
+      avatar: "https://example.com/avatar/johndoe.jpg",
+      phone_number: "1234567890",
+      major: "Computer Science",
+      intake: 2022
+    },
+    {
+      name: "Jane Smith",
+      _id: "17018@student.vgu.edu.vn",
+      avatar: "https://example.com/avatar/janesmith.jpg",
+      phone_number: "0987654321",
+      major: "Business Administration",
+      intake: 2021
+    },
+    {
+      name: "Bob Johnson",
+      _id: "18028@student.vgu.edu.vn",
+      avatar: "https://example.com/avatar/bobjohnson.jpg",
+      phone_number: "555-555-5555",
+      major: "Psychology",
+      intake: 2023
+    }
+  ]
+
 export const courses = [
     {
-      name: "Introduction to Computer Science",
-      description: "Introduction to Computer Science",
-      picture: "https://example.com/intro-to-cs.jpg",
-      semester: "Fall 2023",
-      duration: "12 weeks",
-      lecturer_name: "John Doe",
+      name: "Course A",
+      content: "6020f0ed02e4646ec5a5e825",
+      picture: "http://example.com/image1.jpg",
+      semester: "Fall 2022",
+      duration: "3 months",
+      lecturer_id: "sarahlee@example.com",
       participants: [
         {
-          email: "jane.doe@example.com",
-          progress: 50,
+            email: "17028@student.vgu.edu.vn",
+            progress: 75,
         },
         {
-          email: "jim.smith@example.com",
+            email: "17018@student.vgu.edu.vn",
+            progress: 100,
+        },
+      ],
+    },
+    {
+      name: "Course B",
+      content: "6020f0ed02e4646ec5a5e825",
+      picture: "http://example.com/image2.jpg",
+      semester: "Spring 2022",
+      duration: "4 months",
+      lecturer_id: "sarahlee@example.com",
+      participants: [
+        {
+          email: "17028@student.vgu.edu.vn",
           progress: 75,
         },
         {
-          email: "susan.brown@example.com",
-          progress: 25,
+          email: "18028@student.vgu.edu.vn",
+          progress: 100,
         },
       ],
     },
-    {
-      name: "Data Structures and Algorithms",
-      description: "Data Structures and Algorithms",
-      picture: "https://example.com/data-structures.jpg",
-      semester: "Spring 2024",
-      duration: "10 weeks",
-      lecturer_name: "Jane Smith",
-      participants: [
-        {
-          email: "joe.johnson@example.com",
-          progress: 30,
-        },
-        {
-          email: "karen.jackson@example.com",
-          progress: 60,
-        },
-        {
-          email: "kevin.white@example.com",
-          progress: 80,
-        },
-      ],
-    },
-    {
-      name: "Web Development",
-      description: "Web Development",
-      picture: "https://example.com/web-dev.jpg",
-      semester: "Fall 2024",
-      duration: "14 weeks",
-      lecturer_name: "Jim Green",
-      participants: [
-        {
-          email: "mary.adams@example.com",
-          progress: 90,
-        },
-        {
-          email: "michael.davis@example.com",
-          progress: 50,
-        },
-        {
-          email: "lisa.baker@example.com",
-          progress: 20,
-        },
-      ],
-    },
-  ];
+  ]
