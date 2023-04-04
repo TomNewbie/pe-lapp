bceba06bc7c0d347eb0b0be357b27065f9aecded
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-04-04 10:54:40 +0700

    Update all courses student and lecturer

--- lapp/src/App.js
+++ lapp/src/App.js
@@ -1,9 +1,12 @@
-import { Profile } from "./pages/Student";
+import { Profile, AllCoursesStudent } from "./pages/Student";
+import { AllCoursesLecturer } from "./pages/Lecturer";
 import { Login } from "./pages/common";
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      {/* <Login></Login> */}
+      {/* <AllCoursesStudent></AllCoursesStudent> */}
+      <AllCoursesLecturer></AllCoursesLecturer>
       {/* <Profile></Profile> */}
     </div>
   );
