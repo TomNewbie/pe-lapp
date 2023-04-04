1b4c83f95fc150d9988cde464c965e6f5e83d02a
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-04-04 16:27:57 +0700

    Server: Changed .env mongo uri variable and add mongosh npm script

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -3,6 +3,4 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.8c6w6lm.mongodb.net/?retryWrites=true&w=majority`;
-
-export const db = mongoose.connect(MONGODB_URI);
+export const db = mongoose.connect(process.env.MONGODB_URI!);
