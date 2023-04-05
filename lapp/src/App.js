d52656e49c8d0e6822b0b82f7b6d7057c5c50303
pndquyynh <17937@student.vgu.edu.vn>
2023-04-05 15:43:22 +0700

    Assignment

--- lapp/src/App.js
+++ lapp/src/App.js
@@ -1,10 +1,10 @@
 import { Profile } from "./pages/Student";
 import { Login } from "./pages/common";
+import { Assignment } from "./components";
 function App() {
   return (
     <div className="App">
-      <Login></Login>
-      {/* <Profile></Profile> */}
+      <Assignment></Assignment>
     </div>
   );
 }
---
+++ lapp/src/components/Assignment/index.js
@@ -0,0 +1,18 @@
+const Assignment = () => {
+    return ( 
+        <div class ="w-96 h-fit text-[#1B1C1E] divide-y divide-[#808080]/30">
+            <div class ="flex flex-row justify-between px-2.5 py-2 place-items-center">
+                <img class ="w-6 h-6" src="/Assignment/AssignmentIcon.png" alt="" />
+                <div>
+                    <p>Assignment 1</p>
+                    <p>5/40 submissions</p>
+                </div>
+                <p>Due date: 31/02/2023</p>
+                <img class ="w-4 h-4" src="/Assignment/Expand.png" alt="" />
+                <img class ="w-4 h-4" src="/Assignment/More.png" alt="" />
+            </div>
+        </div>
+    )
+}
+ 
+export default Assignment;
