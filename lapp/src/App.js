5a47641cad24af9488cc97b6a5c4e17babf5fe1e
linh <khanhlinhvng>
2023-04-06 18:20:09 +0700

    update course name

--- lapp/src/App.js
+++ lapp/src/App.js
@@ -1,15 +1,18 @@
-import { NavbarStudent, NavbarLecturer } from "./components";
+// import { NavbarStudent, NavbarLecturer } from "./components";
 // import { Login } from "./pages/common";
 // import { Dropdown, SearchBox } from "./components";
 // import { NavbarCourse} from "./components";
+// import {StudentCourseName} from "./components";
+import {TeacherCourseName} from "./components";
 function App() {
   return (
     <div className="App">
       {/* <Login></Login> */}
       {/* <NavbarStudent></NavbarStudent> */}
-      <NavbarLecturer />
+      {/* <NavbarLecturer /> */}
       {/* <Dropdown></Dropdown> */}
       {/* <NavbarCourse></NavbarCourse> */}
+      <TeacherCourseName></TeacherCourseName>
     </div>
   );
 }
--- lapp/src/components/index.js
+++ lapp/src/components/index.js
@@ -3,4 +3,6 @@ export { default as NavbarStudent } from "./NavbarStudent";
 export { default as NavbarLecturer } from "./NavbarLecturer";
 export { default as Dropdown} from "./Dropdown";
 export { default as SearchBox} from "./SearchBox";
-export { default as NavbarCourse} from "./NavbarCourse"
+export { default as NavbarCourse} from "./NavbarCourse";
+export { default as StudentCourseName} from "./CourseNameCard/StudentCourseName";
+export { default as TeacherCourseName} from "./CourseNameCard/TeacherCourseName";
