5e248a856af99899ddeac6190dcc7805d5772acd
minhquan <17640@student.vgu.edu.vn>
2023-04-06 22:59:38 +0700

    update-exercise-section

--- lapp/src/components/ExerciseSection/index.js
+++ lapp/src/components/ExerciseSection/index.js
@@ -1,9 +1,25 @@
 const ExerciseSection = () => {
   return (
-    <div class="flex flex-row justify-between">
-      <p>Exercise name</p>
-      <p>Due: Monday, 13 February 2023, 12:00 AM</p>
-      <p>N/A</p>
+    <div class="flex flex-row border-4 border-[#5C0120] rounded-2xl text-[#1B1C1E] text-dongle not-italic items-center justify-between px-9 py-5 mt-12 mx-52">
+      <div class="flex flex-row justify-between space-x-11">
+        <img
+          src="/ExerciseSection/upload-file.png"
+          alt="upload file"
+          loading="lazy"
+        />
+        <p class="font-bold text-[37.5px]">Exercise name</p>
+      </div>
+      <div class="flex flex-row justify-between space-x-14">
+        <p class="font-normal text-[25px]">
+          Due: Monday, 13 February 2023, 12:00 AM
+        </p>
+        <img
+          src="/ExerciseSection/tick-image.png"
+          alt="tick image"
+          loading="lazy"
+        />
+        <p class="font-normal text-[25px]">N/A</p>
+      </div>
     </div>
   );
 };
