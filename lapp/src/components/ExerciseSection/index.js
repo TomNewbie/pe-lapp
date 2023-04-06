28428e157acc829096cb3db6b3847ea5f06b89b9
minhquan <17640@student.vgu.edu.vn>
2023-04-06 20:22:00 +0700

    update-exercise-section

--- lapp/src/components/ExerciseSection/index.js
+++ lapp/src/components/ExerciseSection/index.js
@@ -1,8 +1,9 @@
 const ExerciseSection = () => {
   return (
-    <div>
+    <div class="flex flex-row justify-between">
       <p>Exercise name</p>
       <p>Due: Monday, 13 February 2023, 12:00 AM</p>
+      <p>N/A</p>
     </div>
   );
 };
