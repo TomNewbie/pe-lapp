ce6d13090baf2bf3d6cb94690f4f64c9a2ea11c3
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-04-06 10:34:35 +0700

    Server: Changed MONGODB_URI string usage in config/database.ts AGAIN
    Remove MONGO_USER, MONGO_PASSWORD, MONGO_CLUSTER from .env and add
    MONGODB_URI to .env on your local machine. Review the previous commit's
    src/config/database.ts to know what the variable may be.

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,4 +1,3 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.8c6w6lm.mongodb.net/?retryWrites=true&w=majority`;
-export const db = mongoose.connect(MONGODB_URI)
+export const db = mongoose.connect(process.env.MONGODB_URI!);
