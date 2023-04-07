00a932b8b52a40a57f603163764d8babc8d9b0db
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-04-07 09:56:03 +0700

    Custome course heading

--- lapp/src/App.js
+++ lapp/src/App.js
@@ -1,12 +1,7 @@
 // import { Login } from "./pages/common";
 import { CourseHeading } from "./components";
 function App() {
-  return (
-    <div className="App">
-      {/* <Login></Login> */}
-      <CourseHeading></CourseHeading>
-    </div>
-  );
+  return <div className="App">{/* <Login></Login> */}</div>;
 }
 
 export default App;
