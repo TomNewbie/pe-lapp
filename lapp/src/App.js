6ee24bc20c3e000d18347c8e4514778c98039363
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-04-11 01:19:51 +0700

    Update class code

--- lapp/src/App.js
+++ lapp/src/App.js
@@ -4,13 +4,15 @@ import {
   Profile as ProfileTeacher,
 } from "./pages/Lecturer";
 import { Login } from "./pages/common";
+import Code from "./components/Code";
 function App() {
   return (
     <div className="App">
       {/* <Login></Login> */}
       {/* <AllCoursesStudent></AllCoursesStudent> */}
       {/* <AllCoursesLecturer></AllCoursesLecturer> */}
-      <Profile></Profile>
+      {/* <Profile></Profile> */}
+      <Code code="123423"></Code>
     </div>
   );
 }
