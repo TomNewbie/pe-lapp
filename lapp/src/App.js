40b25a91d65c2302a4f897d79fd17e1ca744f562
linh <khanhlinhvng>
2023-04-11 21:08:46 +0700

    custome noti

--- lapp/src/App.js
+++ lapp/src/App.js
@@ -1,13 +1,20 @@
-import { Profile } from "./pages/Student";
-import { Login } from "./pages/common";
-import { UploadMaterial } from "./components";
+// import { Profile } from "./pages/Student";
+// import { Login } from "./pages/common";
+// import { UploadMaterial } from "./components";
 import { Notification} from "./components";
 function App() {
+  const Files=[
+    {name:"Probability"},
+    {name:"Statistic"},
+    {name:"B"},
+    {name:"C"},
+    {name:"D"}
+  ]
   return (
     <div className="App">
       {/* <UploadMaterial></UploadMaterial>
       <Profile></Profile> */}
-      <Notification file={"true"}></Notification>
+      <Notification status={"true"} title={"Probability"} content={"Today we learn Bayes Rules, hope you like the lecture."} Files={Files}></Notification>
     </div>
   );
 }
