1229130d2887a64b1ad7c717c4c04aaa0e732751
linh <khanhlinhvng>
2023-04-11 11:26:59 +0700

    edit dropdown

--- lapp/src/components/Dropdown/index.js
+++ lapp/src/components/Dropdown/index.js
@@ -1,17 +1,35 @@
 import {useState} from "react";
 function Dropdown(){
     const [active, setActive]=useState(0);
+    const handleClick =() =>{
+        if(active===0){
+            setActive(1);
+        }else{
+            setActive(0);
+        }
+    }
     return (
-        <div className={active===0
-            ? ""
-            : <div>
-                <div>
-                    
-                </div>
-            </div>}
-        >
+        <div >
             <img src="/dropdown/more.svg" alt="" className="h-12 w-12 cursor-pointer"
-                onClick={()=>setActive(1)}></img>
+               onClick={()=>handleClick()}></img>
+            {active===0
+            ? <div></div>
+            : <div>
+                <div className="w-40 h-24 border rounded-2xl">
+                    <div className="ml-4 mt-2">
+                        <div className="flex flex-row hover:text-[#B02B3B] cursor-pointer">
+                            <img src="/dropdown/edit.png" alt="" className="w-5 h-5 mt-1"></img>
+                            <div className="text-xl ml-12">Edit</div>
+                        </div>
+                        <div className="flex flex-row mt-3 hover:text-[#B02B3B] cursor-pointer">
+                            <img src="/dropdown/delete.png" alt="" className="w-5 h-5 mt-1"></img>
+                            <div className="text-xl ml-12">Delete</div>
+                        </div>
+                    </div>
+                </div>
+             </div>}
+        
+            
          </div>
     );
   };
