b6940a12e4fe271b14d055901dd14e9e446601ed
minhquan <17640@student.vgu.edu.vn>
2023-04-11 15:19:43 +0700

    fix-profile

--- lapp/src/App.js
+++ lapp/src/App.js
@@ -5,14 +5,18 @@ import {
 } from "./pages/Lecturer";
 import { Login } from "./pages/common";
 import { UploadMaterial } from "./components";
-import { Notification} from "./components";
+import { Notification } from "./components";
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      {/* <Login></Login> */}
       {/* <AllCoursesStudent></AllCoursesStudent> */}
       {/* <AllCoursesLecturer></AllCoursesLecturer> */}
-      {/* <Profile></Profile> */}
+      <Profile
+        name={"Quan"}
+        id={"12345"}
+        email={"asdawd@ssteunsd.wdcom"}
+      ></Profile>
     </div>
   );
 }
