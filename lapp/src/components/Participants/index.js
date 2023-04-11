19a606d6f91b994524ffa77ff631a48b47a154ef
linh <khanhlinhvng>
2023-04-11 14:28:12 +0700

    click to copy

--- lapp/src/components/Participants/index.js
+++ lapp/src/components/Participants/index.js
@@ -1,24 +1,38 @@
+import React from "react";
 const Participants = ({ teacher, students }) => {
   //   const students = [
   //     { url: "/participants-icon/ava.png", name: "A" },
   //     { url: "/participants-icon/ava.png", name: "B" },
   //     { url: "/participants-icon/ava.png", name: "C" },
   //   ];
+  // const [isCopied, setIsCopied]=useState(0);
+ 
   const studentSection = students.map((student) => (
     <div>
       <div className="relative flex flex-row items-center h-16 border-black text-xl">
         <img src={student.url} alt="" className="absolute mx-4 my-5"></img>
         <div className="absolute ml-20 text-2xl">{student.name}</div>
 
-        <a href={student.mail}>
-          <img
+        <div value={student.mail}
+          id="mail">
+          <button onClick={copyToClipboard()}><img
             src="/participants-icon/mail.png"
             alt=""
-            className="absolute right-4 h-5 w-5"></img>
-        </a>
-      </div>
+            className="absolute right-4 h-5 w-5"
+            ></img></button>
+        </div>
+      </div> 
     </div>
+    
   ));
+  function copyToClipboard(){
+    var copyText= document.getElementById("mail");
+    if (copyText !== null){
+      navigator.clipboard.writeText(copyText.value);
+    }
+    
+
+ }
   console.log(students);
   return (
     <div className="ml-40 max-w-full">
@@ -31,10 +45,11 @@ const Participants = ({ teacher, students }) => {
               alt=""
               className="absolute mx-4 my-5"></img>
             <div className="absolute ml-20 text-2xl">{teacher}</div>
-            <img
+            <button><img
               src="/participants-icon/mail.png"
               alt=""
-              className="absolute right-4 h-5 w-5"></img>
+              className="absolute right-4 h-5 w-5"
+              ></img></button>
           </div>
         </div>
         <div className="divide-y">
