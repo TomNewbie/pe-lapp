c5c4652ca31381c61c5087d7b557a697db44d330
minhquan <17640@student.vgu.edu.vn>
2023-04-11 11:03:29 +0700

    custom-courseCardStudent-courseCardTeacher

--- lapp/src/App.js
+++ lapp/src/App.js
@@ -2,15 +2,22 @@ import { Footer } from "./components";
 import { Login } from "./pages/common";
 import { CoursecardStudent } from "./components";
 import { CoursecardTeacher } from "./components";
-import {NavbarStudent} from "./components";
+import { NavbarStudent } from "./components";
 
 function App() {
   return (
     <div className="App">
       <Login></Login>
-      <CoursecardStudent/>
-      <CoursecardTeacher/>
-
+      <CoursecardStudent
+        courseName={"Course name"}
+        lecturerName={"Lecturer's name"}
+        progress={"% Progress"}
+      />
+      {/* <CoursecardTeacher
+        courseName={"Course name"}
+        numberOfParticipants={"#participants"}
+        semester={"semester"}
+      /> */}
     </div>
   );
 }
--- lapp/src/components/CoursecardStudent/index.js
+++ lapp/src/components/CoursecardStudent/index.js
@@ -1,29 +1,43 @@
-const CoursecardStudent = () => {
-    return ( 
-      
-<div class = "card">
+const CoursecardStudent = ({ courseName, lecturerName, progress }) => {
+  return (
+    <div class="card">
+      <div class="max-w-sm bg-[#DE5D83]/50 rounded-3xl h-67 w-44 shadow-xl">
+        <img
+          class="w-fit h-2/3"
+          src="/Coursecard_img/CourseStuPic.png"
+          alt="Course Picture"
+        />
 
-   <div class="max-w-sm bg-[#DE5D83]/50 rounded-3xl h-67 w-44 shadow-xl">
-         <img class = "w-fit h-2/3" src="/Coursecard_img/CourseStuPic.png" alt="Course Picture" />
-   
-   
-   <div class="container content-around">
-      
-      <h1 class= "text-center text-2xl font-bold tracking-tight dark:text-[#1b1c1e]">Course Name</h1> 
-      
-      <div class = "flex flex-row ml-8">
-         <img class ="w-4 h-4" src="/Coursecard_img/human.png" alt="Lecturer: " />
-         <p class="text-center ml-2 tracking-tight dark:text-[#1b1c1e]"> Lecturer's Name </p>
+        <div class="container content-around">
+          <h1 class="text-center text-2xl font-bold tracking-tight dark:text-[#1b1c1e]">
+            {courseName}
+          </h1>
+
+          <div class="flex flex-row ml-8">
+            <img
+              class="w-4 h-4"
+              src="/Coursecard_img/human.png"
+              alt="Lecturer: "
+            />
+            <p class="text-center ml-2 tracking-tight dark:text-[#1b1c1e]">
+              {lecturerName}
+            </p>
+          </div>
+
+          <div class="flex flex-row ml-8">
+            <img
+              class="w-4 h-4"
+              src="/Coursecard_img/progress.png"
+              alt="Progress: "
+            />
+            <p class="text-center ml-2 tracking-tight dark:text-[#1b1c1e]">
+              {progress}
+            </p>
+          </div>
+        </div>
       </div>
-            
-      <div class = "flex flex-row ml-8">
-         <img class ="w-4 h-4" src="/Coursecard_img/progress.png" alt="Progress: " />
-         <p class="text-center ml-2 tracking-tight dark:text-[#1b1c1e]"> % Progress</p>
-      </div>  
-   </div>
-   </div>
-</div>
-   );
-}
- 
+    </div>
+  );
+};
+
 export default CoursecardStudent;
