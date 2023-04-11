1a209a8c57c4e666190c40887714c56be70bd154
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-04-11 09:25:55 +0700

    First commit

--- lapp/src/components/Announce/index.js
+++ lapp/src/components/Announce/index.js
@@ -1,5 +1,21 @@
 const Announce = () => {
-  return <div>Announce </div>;
+  return (
+    <div className="flex justify-between w-3/4 bg-white drop-shadow-xl rounded-xl">
+      <img
+        src="/Announce/comment1.png"
+        alt="commentsection"
+        width={36}
+        height={362}
+      />
+      <p className="text-[45px]">Announce something ...</p>
+      <img
+        src="/Announce/comment2.png"
+        alt="commentsection"
+        width={25}
+        height={25}
+      />
+    </div>
+  );
 };
 
 export default Announce;
