087e37d6bfff8f10838317411c48a265acea5852
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-04-11 11:02:11 +0700

    Update App.js

--- lapp/src/App.js
+++ lapp/src/App.js
@@ -4,16 +4,10 @@ import {
   Profile as ProfileTeacher,
 } from "./pages/Lecturer";
 import { Login } from "./pages/common";
-import { Assignment } from "./components";
 function App() {
-  const students = [
-    { url: "/participants-icon/ava.png", name: "A", mail: "ava.gmail.com" },
-    { url: "/participants-icon/ava.png", name: "B", mail: "ava.gmail.com" },
-    { url: "/participants-icon/ava.png", name: "C", mail: "ava.gmail.com" },
-  ];
   return (
     <div className="App">
-      {/* <Login></Login> */}
+      <Login></Login>
       {/* <AllCoursesStudent></AllCoursesStudent> */}
       {/* <AllCoursesLecturer></AllCoursesLecturer> */}
       {/* <Profile></Profile> */}
