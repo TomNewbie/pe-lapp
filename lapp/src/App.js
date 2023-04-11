bf6eef3f9a504fd089ca7ac68b168c213454de22
linh <khanhlinhvng>
2023-04-11 15:20:32 +0700

    edit recordtable

--- lapp/src/App.js
+++ lapp/src/App.js
@@ -2,14 +2,15 @@ import { Profile } from "./pages/Student";
 import { Login } from "./pages/common";
 import { RecordTable } from "./components";
 function App() {
+  const students=[
+    {Name:"le hoang kim thanh",id:"1",submittime:"20:00 4/4/2023", status:"On time", grade:"8"},
+    {Name:"B",id:"2",submittime:"20:00 5/4/2023", status:"Late", grade:"8"},
+    {Name:"C",id:"3",submittime:"21:00 5/4/2023", status:"Missing", grade:"8"}
+  ]
   return (
     <div className="App">
       <RecordTable
-        Name={"Le Hoang Kim Thanh"}
-        id="18047"
-        submittime="20:00 4/4/2023"
-        status="On time"
-        grade="8"></RecordTable>
+       students={students}></RecordTable>
     </div>
   );
 }
--- lapp/src/components/RecordTable/index.js
+++ lapp/src/components/RecordTable/index.js
@@ -1,10 +1,40 @@
-const RecordTable = ({ Name, id, submittime, file, grade, status }) => {
+const RecordTable = ({ students }) => {
+  const index=students.map((student)=>(
+   
+        <tr class ="">
+          <td class="px-6 py-4 text-center border border-slate-300 ">
+            {student.Name}
+          </td>
+          <td class="px-6 py-4 text-center border border-slate-300">
+            {student.id}
+          </td>
+          <td class="px-6 py-4 text-center border border-slate-300">
+            {student.submittime}
+          </td>
+          <td
+            class={student.status === "on-time"
+                    ? "px-6 py-4 text-center text-[#1B1C1E] border border-slate-300"
+                    : student.status === "missing"
+                    ? "text-[#7F1734]"
+                    : "text-[#FBA70E]"
+                }>
+                {student.status}
+          </td>
+          <td class="px-6 py-4 text-center border border-slate-300">
+            {student.file}
+          </td>
+          <td class="px-6 py-4 text-center border border-slate-300">
+            {student.grade}
+          </td>
+        </tr>
+  
+  ));
   return (
     <div class="flex flex-col h-screen">
       <div class="flex-grow overflow-auto">
         <table class="relative w-full border-collapse border border-slate-500 ">
           <thead>
-            <tr>
+            <tr class ="h-fit">
               <th class="sticky top-0 px-6 py-3 border border-slate-300 text-[#7F1734] bg-[#F4C2C2]/40">
                 Name
               </th>
@@ -27,33 +57,7 @@ const RecordTable = ({ Name, id, submittime, file, grade, status }) => {
             </tr>
           </thead>
           <tbody class="divide-y">
-            <tr>
-              <td class="px-6 py-4 text-center border border-slate-300">
-                {Name}
-              </td>
-              <td class="px-6 py-4 text-center border border-slate-300">
-                {id}
-              </td>
-              <td class="px-6 py-4 text-center border border-slate-300">
-                {submittime}
-              </td>
-              <td
-                class={
-                  status === "on-time"
-                    ? "px-6 py-4 text-center text-[#1B1C1E] border border-slate-300"
-                    : status === "missing"
-                    ? "text-[#7F1734]"
-                    : "text-[#FBA70E]"
-                }>
-                {status}
-              </td>
-              <td class="px-6 py-4 text-center border border-slate-300">
-                {file}
-              </td>
-              <td class="px-6 py-4 text-center border border-slate-300">
-                {grade}
-              </td>
-            </tr>
+            {index}
           </tbody>
         </table>
       </div>
