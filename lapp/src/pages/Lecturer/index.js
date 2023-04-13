004d604bdc78a9bfc97abe43820e85ca9a35fe90
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-04-13 10:07:22 +0700

    Delete wrong import

--- lapp/src/pages/Lecturer/index.js
+++ lapp/src/pages/Lecturer/index.js
@@ -2,4 +2,3 @@ export { default as AllCoursesLecturer } from "./AllCourses";
 export { default as Profile } from "./Profile";
 export { default as ExerciseDetail } from "./ExerciseDetail";
 export { default as CoursePage } from "./CoursePage";
-export { default as CoursePage } from "./CoursePage";
