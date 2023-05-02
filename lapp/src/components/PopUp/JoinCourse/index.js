8f2d988c59c1dde99d256b8b20a4782ee3f49516
linh <khanhlinhvng>
2023-05-02 18:27:19 +0700

    change size

--- lapp/src/components/PopUp/JoinCourse/index.js
+++ lapp/src/components/PopUp/JoinCourse/index.js
@@ -2,12 +2,12 @@ import CustomButton from "../../CustomButton";
 
 const JoinCourse = () => {
   return (
-    <div class="flex flex-col justify-between space-y-4 px-5 py-2 w-60 h-36 bg-[#FFFCF7] rounded-xl shadow-xl place-items-center">
-      <p class="text-3xl h-4">Enter Course Code</p>
+    <div class="flex flex-col justify-between space-y-4 px-5 py-2 w-[800px] h-fit bg-[#FFFCF7] rounded-xl shadow-xl place-items-center border">
+      <p class="text-3xl mt-2">Enter Course Code</p>
       <input
         type="text"
-        class="w-52 h-8 bg-[#9F5F5F]/30 rounded-xl text-2xl px-3"></input>
-        <CustomButton variant={"filled"} className={"px-8 py-0"} text={"Join"}></CustomButton>
+        class="w-[600px] h-12 bg-[#9F5F5F]/30 rounded-xl text-2xl px-3"></input>
+        <div className="pb-2"><CustomButton variant={"filled"} className={"px-6 py-0"} text={"Join"}></CustomButton></div>
       
     </div>
   );
