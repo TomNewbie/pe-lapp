875d6d380cb44a6ccdac94c2dd804c6e603275b2
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-05-06 01:24:08 +0700

    Client: Add login service

--- lapp/src/services/auth.ts
+++ lapp/src/services/auth.ts
@@ -1,5 +1,12 @@
 import { apiRequest } from ".";
 
+export const login = () => {
+  const { location } = window;
+  location.href = `/api/auth/login?redirect=${encodeURIComponent(
+    location.href
+  )}`;
+};
+
 export const logout = async () => {
   await apiRequest("/api/auth/logout");
 };
