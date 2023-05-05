91151802fa683d0f7d8ab379f382710ce07168e9
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-05-06 01:26:43 +0700

    Client: Update Auth context for the auth feature to be functional

--- lapp/src/components/auth.js
+++ lapp/src/components/auth.js
@@ -1,17 +1,19 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useCallback, useContext } from "react";
+import { useAPI } from "../hooks/useAPI";
+import { login, logout as _logout } from "../services/auth";
+
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const login = (user) => {
-    setUser(user);
-  };
-  const logout = () => {
-    setUser(null);
-  };
+  const { data: user, pending, refresh } = useAPI({ path: "/api/user/info" });
+
+  const logout = useCallback(async () => {
+    await _logout();
+    await refresh();
+  }, [refresh]);
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, pending, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
--- lapp/src/pages/common/Login/index.js
+++ lapp/src/pages/common/Login/index.js
@@ -4,14 +4,9 @@ import { useAuth } from "../../../components/auth";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [user, setUser] = useState("thanh");
   const auth = useAuth();
-  const navigate = useNavigate();
-  const location = useLocation();
-  const redirectPath = location.state?.path || "/allcourses";
   const handleLogin = () => {
-    auth.login(user);
-    navigate(redirectPath, { replace: true });
+    auth.login();
   };
   return (
     <div>
