d7751f0df96e87c42872bcf3a525f4a62e77527c
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-05-14 01:16:04 +0700

    Update home

--- lapp/src/pages/common/Home/index.js
+++ lapp/src/pages/common/Home/index.js
@@ -1,4 +1,4 @@
-import { Footer, KeyFeature } from "../../../components";
+import { Footer, KeyFeature, NumCountUp } from "../../../components";
 import FeedbackSection from "../../../components/Feedback";
 import Login from "../Login";
 /**
@@ -12,6 +12,7 @@ const Home = () => {
       <div className="flex justify-center mt-40 mb-12">
         <KeyFeature />
       </div>
+      <NumCountUp />
       <FeedbackSection />
       <Footer></Footer>
     </div>
