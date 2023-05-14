4f344a710a942624f855e35659c483ffda9abab1
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-05-14 09:50:14 +0700

    Update home

--- lapp/src/components/NumCountUp/index.js
+++ lapp/src/components/NumCountUp/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Confetti from "react-confetti";
 
 const NumCountUp = () => {
   const [upto, setUpto] = useState(0);
@@ -21,27 +20,16 @@ const NumCountUp = () => {
     });
   };
 
-  const confettiColors = ["#cc6666", "#00ff00", "#0000ff"]; // Customize the confetti colors
-
   return (
-    <div style={{ textAlign: "center" }}>
+    <div style={{ textAlign: "center" }} className="text-8xl">
       <div id="counter">
+        <span role="img" aria-label="users">
+          🎉
+        </span>
         {upto}
-        {upto === desiredNumber && (
-          <Confetti
-            width={window.innerWidth}
-            height={window.innerHeight}
-            recycle={false} // Disable confetti recycling
-            numberOfPieces={200} // Set the number of confetti pieces
-            gravity={0.1} // Set the gravity of confetti animation
-            friction={0.99} // Set the friction of confetti animation
-            colors={confettiColors} // Customize the confetti colors
-            confettiSource={{ x: 0, y: -100, w: window.innerWidth, h: 0 }} // Customize the confetti source
-            initialVelocityX={10} // Set initial X-axis velocity of confetti
-            initialVelocityY={30} // Set initial Y-axis velocity of confetti
-            run={5} // Set the duration of confetti animation in seconds
-          />
-        )}
+        <span role="img" aria-label="users">
+          🎉
+        </span>
       </div>
       <h1>Users using LAPP</h1>
     </div>
