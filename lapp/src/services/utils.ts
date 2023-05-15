9bbf26f7eaa75929ceb3e0a411ea7d08082729da
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-05-15 14:40:41 +0700

    Client: Update toFormData util to handle edge cases

--- lapp/src/services/utils.ts
+++ lapp/src/services/utils.ts
@@ -7,6 +7,16 @@ export function toFormData(obj: object): FormData {
   const formData = new FormData();
 
   for (const [key, val] of Object.entries(obj)) {
+    if (val == null) {
+      // skip null and undefined values
+      continue;
+    }
+
+    if (val instanceof Date) {
+      formData.set(key, val.toISOString());
+      continue;
+    }
+
     if (typeof val === "string" || !isIterable(val)) {
       formData.set(key, val);
       continue;
