0f693f81f5aa86f8e59cae71e47b681f5a55d277
pndquyynh <17937@student.vgu.edu.vn>
2023-05-15 14:23:24 +0700

    Fix exercise

--- lapp/src/pages/Student/Exercise/index.js
+++ lapp/src/pages/Student/Exercise/index.js
@@ -48,14 +48,16 @@ const ExerciseDetail = () => {
   return (
     <div class="text-[#1B1C1E] flex flex-col bg-[#FFFAF0]">
       <NavbarStudent></NavbarStudent>
+
+      {/* Course name card */}
       <div class="mb-16">
         <StudentCourseName
           name={course.name}
-          teacher={course.teacher}
-        ></StudentCourseName>
+          teacher={course.teacher}></StudentCourseName>
       </div>
 
-      <div class="flex flex-row items-center justify-around space-x-5">
+      {/* Instruction part */}
+      <div class="flex flex-row items-center justify-around space-x-5 mx-16">
         <div class="basis-2/3">
           <div class=" flex flex-col px-2">
             <p class="text-bold text-7xl text-left">{data.name}</p>
@@ -69,16 +71,16 @@ const ExerciseDetail = () => {
               <div className="flex flex-col ml-4 space-y-4">
                 <div className="text-3xl font-light">{data.description}</div>
               </div>
-              <div className="flex flex-row ml-4 space-x-4">
+              <div className="flex flex-row ml-4 space-x-4 place-content-between ">
                 {data.exercise_files?.map((file) => (
                   <a href={file.url}>
-                    <div className="flex px-2 py-3 border border-black w-fit rounded-2xl">
+                    {/* Uploaded file section */}
+                    <div className="flex px-2 py-3 border border-black w-96 rounded-2xl">
                       <img
                         src="/notification/upload.svg"
                         alt=""
-                        className="w-9 h-9"
-                      ></img>
-                      <div className="ml-3 text-3xl font-semibold">
+                        className="w-9 h-9"></img>
+                      <div className="ml-3 text-3xl font-semibold truncate">
                         {file.name}
                       </div>
                     </div>
@@ -89,21 +91,20 @@ const ExerciseDetail = () => {
           </div>
         </div>
 
-        <div class="mt-32">
-          <SubmitEx
-            handleSubmit={() => {
-              console.log("submit");
-            }}
-          ></SubmitEx>
+        {/* Grading & submit exercise section */}
+        <div className="flex flex-col items-center basis-1/4">
+          <div>
+            <CommentSection grade={comment.grade}></CommentSection>
+          </div>
+          <div class="">
+            <SubmitEx
+              handleSubmit={() => {
+                console.log("submit");
+              }}></SubmitEx>
+          </div>
         </div>
       </div>
 
-      <div class="pl-72 mt-24 mb-24">
-        <CommentSection
-          grade={comment.grade}
-          comment={comment.content}
-        ></CommentSection>
-      </div>
       <Footer></Footer>
     </div>
   );
