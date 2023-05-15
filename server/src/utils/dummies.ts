9c5e3385a246fc0f68e5ddfb21154bdbc5056e64
NguyenLe1605 <lhdnguyen2002@gmail.com>
2023-05-15 12:31:55 +0700

    Add dummy course data

--- server/src/utils/dummies.ts
+++ server/src/utils/dummies.ts
@@ -32,15 +32,79 @@ const generateLecturer = () => {
     const lecturerDomain = "vgu.edu.vn";
     const lecturerId = faker.person.firstName();
     const email = generateEmail(lecturerId, lecturerDomain);
-    const falcuties = ["engineering", "business"]
+    const falcuty = "Engineering";
     return {
         _id: lecturerId,
         email,
         name: faker.person.fullName({firstName: lecturerId}),
-        falcuty: faker.helpers.arrayElement(falcuties),
+        falcuty,
     }   
 }
 
 export const lecturers = faker.helpers.multiple(generateLecturer, {
     count: 10,
 })
+
+const files = [
+    {
+        "name":"01_Ch1 Introduction.pdf",
+        "url":"https://firebasestorage.googleapis.com/v0/b/pe-lapp-384707.appspot.com/o/production%2Fdefault%2F01_Ch1%20Introduction.pdf?alt=media&token=25998c44-7c45-4e36-8ce7-3afd63816768",
+        "refPath":"production/default/01_Ch1 Introduction.pdf"
+    },
+    {
+        "name":"02_Ch2 Software Processes.pdf",
+        "url":"https://firebasestorage.googleapis.com/v0/b/pe-lapp-384707.appspot.com/o/production%2Fdefault%2F02_Ch2%20Software%20Processes.pdf?alt=media&token=ec6d6ddb-52d0-493f-8d22-90445c3a1b15",
+        "refPath":"production/default/02_Ch2 Software Processes.pdf"
+    },
+    {
+        "name":"7 - Deadlock.pdf",
+        "url":"https://firebasestorage.googleapis.com/v0/b/pe-lapp-384707.appspot.com/o/production%2Fdefault%2F7%20-%20Deadlock.pdf?alt=media&token=13eb8012-e9d7-4045-ba5c-af6dff245974",
+        "refPath":"production/default/7 - Deadlock.pdf"
+    },
+    {
+        "name":"Activity_Diagram.pdf",
+        "url":"https://firebasestorage.googleapis.com/v0/b/pe-lapp-384707.appspot.com/o/production%2Fdefault%2FActivity_Diagram.pdf?alt=media&token=d3f32e87-fbfa-410d-9478-b8d94053ebac",
+        "refPath":"production/default/Activity_Diagram.pdf"
+    },
+]
+
+const generateContent = () => {
+    return {
+        _id: faker.database.mongodbObjectId(),
+        title: faker.lorem.sentence(),
+        files: faker.helpers.arrayElements(files),
+        body: faker.lorem.paragraph(),
+    }
+}
+
+export const contents = faker.helpers.multiple(generateContent, {
+    count: 24,
+})
+
+const courseNames = [
+    "Software Engineering",
+    "Operating System",
+    "Computer Network",
+    "Realtime system",
+    "Distributed System",
+    "Discrete Math",
+    "Data structure and Algorithm",
+    "IT security",
+]
+
+const generateCourse = (name: string) => {
+    const contentIds = contents.map(content => content._id);
+    const semesters = ["SS2023", "WS2023"];
+    const lecturerIds = lecturers.map(lecturer => lecturer._id);
+    const participantIds = students.map(student => student._id);
+    return {
+        _id: faker.database.mongodbObjectId(),
+        name,
+        contents: faker.helpers.arrayElements(contentIds),
+        semester: faker.helpers.arrayElement(semesters),
+        lecturer_id: faker.helpers.arrayElement(lecturerIds),
+        participants: faker.helpers.arrayElements(participantIds),
+    }
+}
+
+export const courses = courseNames.map(name => generateCourse(name));
--- server/src/utils/generate_data.ts
+++ server/src/utils/generate_data.ts
@@ -4,7 +4,8 @@ const envPath = path.resolve(__dirname, "..", "..", ".env");
 dotenv.config({ path: envPath });
 import { db } from '../config/database'
 import { Student, Lecturer } from '../api/model/user'
-import { students, lecturers } from './dummies';
+import { CourseContent, Course } from "../api/model/course";
+import { students, lecturers, contents, courses } from './dummies';
 
 const insertData = async(Model: any, data: any) => {
   try {
@@ -28,9 +29,13 @@ db.then(async () => {
   if (process.argv[2] === '-i') {
     await insertData(Student,students);
     await insertData(Lecturer, lecturers);
+    await insertData(CourseContent, contents);
+    await insertData(Course, courses);
   } else {
     await deleteData(Student);
     await deleteData(Lecturer);
+    await deleteData(CourseContent);
+    await deleteData(Course);
   }
   process.exit();
 }).catch(err => {
